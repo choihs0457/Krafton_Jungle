bea0bfcd6000f3744d94f4825d558d47b41f7457
choihs0457 <85881597+choihs0457@users.noreply.github.com>
2022-11-13 22:33:41 +0900

    Week03 트리순회(백준) 코드 추가

---
+++ Week3/트리 순회.py
@@ -0,0 +1,36 @@
+import sys
+imput = sys.stdin.readline
+
+N = int(input())
+tree = {}
+
+def preorder(root):
+    if root != '.':
+        print(root, end='')
+        preorder(tree[root][0])
+        preorder(tree[root][1])
+ 
+ 
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])
+        print(root, end='')
+        inorder(tree[root][1])
+ 
+ 
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0])
+        postorder(tree[root][1])
+        print(root, end='')
+
+
+for _ in range(N):
+    root, left, right = input().strip().split()
+    tree[root] = [left, right]
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
